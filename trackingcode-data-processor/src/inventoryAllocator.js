8e8b79a0b23859fed58359f3d12094968c538f63
jiehao <jiehao.liu@mail.utoronto.ca>
2020-02-09 02:58:02 -0500

    optimized the solution and updated README

--- trackingcode-data-processor/src/inventoryAllocator.js
+++ trackingcode-data-processor/src/inventoryAllocator.js
@@ -5,46 +5,45 @@ class InventoryAllocator{
     }
 
     allocateInventory(){
-        let result = []                 // final result to be returned 
-        let orderCopy = this.order      // a copy of order information which will be updated each time an assignment is made
+        let result = []                                           // final result to be returned 
+        let orderCopy = JSON.parse(JSON.stringify(this.order))    // a deep copy of order information which will be updated each time an assignment is made
         
-        /* check if spliting can be avoided for the first time we visit all warehouses */
-        Object.keys(orderCopy).forEach((key)=>{      
-            this.inventoryInfo.forEach((warehouse)=>{
-                const inventory = warehouse.inventory     // inventory data, e.g. "apple": 1, "banana":5
+        for (let key of Object.keys(orderCopy)){                  // for each item on the order
+            let splitDelivery = []
+            for(let warehouse of this.inventoryInfo){             // for each warehouse
+                const inventory = warehouse.inventory             // inventory data, e.g. "apple": 1, "banana":5
                 if( typeof orderCopy[key]!=='undefined' && inventory[key]>0){   // if the current warehouse has the item on the order list
-                    if(orderCopy[key]<=inventory[key]){   // if the stock is enough
+                    if((this.order)[key]<=inventory[key]){        // if the stock is enough, note here we use the original order!
                         let ChosenWarehouse = {}
-                        ChosenWarehouse[warehouse.name]={[key]:orderCopy[key]}
+                        ChosenWarehouse[warehouse.name]={[key]:(this.order)[key]}
                         result.push(ChosenWarehouse)
-                        delete orderCopy[key]             // remove the fulfilled item from order list
-                    }    
-                }
-            })
-        })
-
-        /* For the second time, we check if there is enough inventory across all warehouses*/
-        Object.keys(orderCopy).forEach((key)=>{      
-            this.inventoryInfo.forEach((warehouse)=>{
-                const inventory = warehouse.inventory     // inventory data, e.g. "apple": 1, "banana":5
-                if( typeof orderCopy[key]!=='undefined' && inventory[key]>0){   // if the current warehouse has the item on the order list
-                    if(orderCopy[key]<=inventory[key]){   // if the stock is enough
-                        let ChosenWarehouse = {}
-                        ChosenWarehouse[warehouse.name]={[key]:orderCopy[key]}
-                        result.push(ChosenWarehouse)
-                        delete orderCopy[key]             // remove the fulfilled item from order list
-                    } 
-                    else{                                 // if the current warehouse does not have enough stock
+                        delete orderCopy[key]                     // remove the fulfilled item from orderCopy list, not original order!
+                        break                                     // this item has been fulfilled, no need to check the rest warehouse
+                    }
+                    else if(orderCopy[key]<=inventory[key]){      // split case, some items have been splited by preivous warehouse(s)
+                        if(orderCopy[key]!=0){
+                            let ChosenWarehouse = {}
+                            ChosenWarehouse[warehouse.name]={[key]:orderCopy[key]}
+                            splitDelivery.push(ChosenWarehouse)
+                            orderCopy[key] = 0                    // Mark it zero for checking if split is necessary
+                        }             
+                    }
+                    else{                                         // if the current warehouse does not have enough stock
                         let ChosenWarehouse = {}
                         ChosenWarehouse[warehouse.name]={[key]:inventory[key]}
-                        result.push(ChosenWarehouse)
-                        orderCopy[key] -= inventory[key]  // update orderCopy and lessen the number of the item
-                    }   
+                        splitDelivery.push(ChosenWarehouse)
+                        orderCopy[key] -= inventory[key]          // update orderCopy and lessen the number of the item
+                    }
                 }
-            })
-        })
+            }
+            // split has to be made if orderCopy[key] still exists
+            if(orderCopy[key] ==0 ){
+                delete orderCopy[key]
+                result = result.concat(splitDelivery)
+            }  
+        }
 
-        if(Object.keys(orderCopy).length!==0) result=[]    // Not enough inventory -> no allocations and clear result!
+        if(Object.keys(orderCopy).length!==0) result=[]          // Not enough inventory -> no allocations and clear result!
 
         return result
     }
